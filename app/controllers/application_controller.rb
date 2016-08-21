0561809b345bda4ee29b933989514cf6e5e06ae3
satomicchy <satomi_t_sora@yahoo.co.jp>
2016-08-22 04:20:15 +0900

    add basic authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :contact
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+
   def contact
     @lodge = Lodge.new
   end
