77071bfb11f1f490ec8155b66397fd49cd12cd56
satomicchy <satomi_t_sora@yahoo.co.jp>
2016-08-22 04:43:46 +0900

    set smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["MAIL_USER_NAME"],
+  :password => ENV["MAIL_USER_PASS"],
+  :domain => 'kitayama-trading.net',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
